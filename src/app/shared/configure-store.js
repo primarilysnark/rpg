e5c84e671724cd10a6c6f5f4c9d2058ab08260df
Primarily Snark <starkey.joshua@gmail.com>
2016-09-09 21:24:49 -0700

    Add promise middleware to Redux.

--- src/app/shared/configure-store.js
+++ src/app/shared/configure-store.js
@@ -3,10 +3,43 @@ import thunkMiddleware from 'redux-thunk';
 
 import { rootReducer } from './reducers';
 
+function createPromiseMiddleware(...callbackArgs) {
+  return () => next => action => {
+    const { promise, types, ...rest } = action;
+
+    if (!promise) {
+      return next(action);
+    }
+
+    next({
+      ...rest,
+      type: types.processing,
+    });
+
+    return new Promise(resolve => {
+      if (typeof promise === 'function') {
+        resolve(promise(...callbackArgs));
+      }
+
+      resolve(promise);
+    })
+      .then(result => next({
+        ...rest,
+        result,
+        type: types.success,
+      }), error => next({
+        ...rest,
+        error,
+        type: types.error,
+      }));
+  };
+}
+
 export function configureStore(initialState, middlewareApi, storeConfig = []) {
   const createStoreWithMiddleware = compose(
     applyMiddleware(
-      thunkMiddleware
+      thunkMiddleware,
+      createPromiseMiddleware()
     ),
     ...storeConfig
   )(createStore);
