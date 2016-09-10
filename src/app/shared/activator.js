2dbf9a06e43e3ea731dca09deaf32e4b2619fbdb
Primarily Snark <starkey.joshua@gmail.com>
2016-09-09 22:24:28 -0700

    Fix nit in activator.

--- src/app/shared/activator.js
+++ src/app/shared/activator.js
@@ -2,7 +2,9 @@ export function activate({ components }, activationArgs) {
   return Promise.all(components.map(component => {
     if (component.activate) {
       return component.activate(...activationArgs);
-    } else if (component.WrappedComponent && component.WrappedComponent.activate) {
+    }
+
+    if (component.WrappedComponent && component.WrappedComponent.activate) {
       return component.WrappedComponent.activate(...activationArgs);
     }
 
