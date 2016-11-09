62cdc3b271ea4d02eb52f24db7da1e14b6b41ceb
Primarily Snark <starkey.joshua@gmail.com>
2016-11-09 13:01:21 -0800

    Convert alignments from MongoDB to MySQL.

--- src/server/controllers/alignments.js
+++ src/server/controllers/alignments.js
@@ -1,5 +1,10 @@
-import { Alignment, prettifyAlignment } from '../models';
 import { sendBadRequest, sendNotFound } from './util';
+import {
+  deleteAlignmentById,
+  fetchAlignmentById,
+  fetchAlignments,
+  saveAlignment,
+} from '../models';
 
 export function createAlignment(req, res) {
   if (req.body.description == null) {
@@ -14,64 +19,70 @@ export function createAlignment(req, res) {
     return sendBadRequest(res, 'Alignments must have a tagline.');
   }
 
-  return new Alignment({
+  return saveAlignment(req.connection, {
     description: req.body.description,
     name: req.body.name,
     tagline: req.body.tagline,
   })
-    .save()
     .then(alignment => res.status(201).json({
-      data: prettifyAlignment(alignment),
+      data: alignment,
     }))
     .catch(err => sendBadRequest(res, err));
 }
 
 export function getAlignment(req, res) {
   if (req.params.alignmentId == null) {
-    return sendBadRequest(res);
+    return res.status(400).send();
   }
 
-  return Alignment.findById(req.params.alignmentId)
+  const alignmentId = parseInt(req.params.alignmentId, 10);
+  if (isNaN(alignmentId)) {
+    if (req.params.alignmentId == null) {
+      return res.status(404).send();
+    }
+  }
+
+  return fetchAlignmentById(req.connection, alignmentId)
     .then(alignment => {
       if (alignment == null) {
         return sendNotFound(res);
       }
 
       return res.status(200).json({
-        data: prettifyAlignment(alignment),
+        data: alignment,
       });
     })
     .catch(() => sendNotFound(res));
 }
 
 export function getAlignments(req, res) {
-  if (req.query.search != null && typeof req.query.search !== 'string') {
-    return sendBadRequest(res, 'Search must be a string');
-  }
-
-  return Alignment.find({
-    name: new RegExp(`^${req.query.search || ''}.*`, 'i'),
-  })
+  return fetchAlignments(req.connection)
     .then(alignments => res.status(200).json({
-      data: alignments.map(prettifyAlignment),
+      data: alignments,
     }))
     .catch(err => res.status(500).send(err));
 }
 
 export function deleteAlignment(req, res) {
   if (req.params.alignmentId == null) {
-    return sendBadRequest(res);
+    return res.status(400).send();
   }
 
-  return Alignment.findById(req.params.alignmentId)
+  const alignmentId = parseInt(req.params.alignmentId, 10);
+  if (isNaN(alignmentId)) {
+    if (req.params.alignmentId == null) {
+      return res.status(404).send();
+    }
+  }
+
+  return fetchAlignmentById(req.connection, alignmentId)
     .then(alignment => {
       if (alignment == null) {
-        return sendNotFound(res);
+        return res.status(404).send();
       }
 
-      return Alignment.where({ _id: req.params.alignmentId })
-        .remove();
+      return deleteAlignmentById(req.connection, alignment.id);
     })
     .then(() => res.status(204).send())
-    .catch(() => sendNotFound(res));
+    .catch(err => res.status(404).send(err));
 }
--- src/server/models/alignment.js
+++ src/server/models/alignment.js
@@ -1,27 +1,90 @@
-/* eslint new-cap: 0 */
-import mongoose from 'mongoose';
-
-export const Alignment = mongoose.model('Alignment', mongoose.Schema({
-  description: {
-    type: [String],
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  tagline: {
-    type: String,
-    required: true,
-  },
-}));
-
-export function prettifyAlignment(alignment) {
+function mapDatabaseToPrettyAlignment(alignment) {
   return {
-    id: alignment._id.toString(),
+    id: alignment.id,
+    description: alignment.description.split('\n\n'),
     name: alignment.name,
-    description: alignment.description,
     tagline: alignment.tagline,
   };
+}
+
+function mapPrettyAlignmentToDatabase(alignment) {
+  return {
+    id: alignment.id,
+    description: alignment.description.join('\n\n'),
+    name: alignment.name,
+    tagline: alignment.tagline,
+  };
+}
+
+export function deleteAlignmentById(connection, id) {
+  return new Promise((resolve, reject) => {
+    connection.query('DELETE FROM alignments WHERE id = ?', [
+      id,
+    ], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(results);
+    });
+  });
+}
+
+export function fetchAlignmentById(connection, id) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM alignments a WHERE a.id = ?', [
+      id,
+    ], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(results);
+    });
+  })
+    .then(results => {
+      if (results.length === 0) {
+        return null;
+      }
+
+      return mapDatabaseToPrettyAlignment(results[0]);
+    });
+}
+
+export function fetchAlignments(connection) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM alignments', (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(results);
+    });
+  })
+    .then(results => {
+      if (results.length === 0) {
+        return [];
+      }
+
+      return results.map(alignment => mapDatabaseToPrettyAlignment(alignment));
+    });
+}
+
+export function saveAlignment(connection, prettyAlignment) {
+  const databaseAlignment = mapPrettyAlignmentToDatabase(prettyAlignment);
+
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO alignments (name, tagline, description) VALUES (?, ?, ?)', [
+      databaseAlignment.name,
+      databaseAlignment.tagline,
+      databaseAlignment.description,
+    ], (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(result);
+    });
+  })
+    .then(result => fetchAlignmentById(connection, result.insertId));
 }
