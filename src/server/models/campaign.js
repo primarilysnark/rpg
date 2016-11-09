c2e594ba5931e3683620754c59802dcd50948cd5
Primarily Snark <starkey.joshua@gmail.com>
2016-11-09 12:18:40 -0800

    Convert campaigns from MongoDB to MySQL.

--- src/server/models/campaign.js
+++ src/server/models/campaign.js
@@ -1,13 +1,84 @@
-/* eslint new-cap: 0 */
-import mongoose from 'mongoose';
-
-export const Campaign = mongoose.model('Campaign', mongoose.Schema({
-  name: String,
-}));
-
-export function prettifyCampaign(campaign) {
+function mapDatabaseToPrettyCampaign(campaign) {
   return {
-    id: campaign._id,
+    id: campaign.id,
     name: campaign.name,
   };
+}
+
+function mapPrettyCampaignToDatabase(campaign) {
+  return {
+    id: campaign.id,
+    name: campaign.name,
+  };
+}
+
+export function deleteCampaignById(connection, id) {
+  return new Promise((resolve, reject) => {
+    connection.query('DELETE FROM campaigns WHERE id = ?', [
+      id,
+    ], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(results);
+    });
+  });
+}
+
+export function fetchCampaignById(connection, id) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM campaigns c WHERE c.id = ?', [
+      id,
+    ], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(results);
+    });
+  })
+    .then(results => {
+      if (results.length === 0) {
+        return null;
+      }
+
+      return mapDatabaseToPrettyCampaign(results[0]);
+    });
+}
+
+export function fetchCampaigns(connection) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM campaigns', (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(results);
+    });
+  })
+    .then(results => {
+      if (results.length === 0) {
+        return [];
+      }
+
+      return results.map(campaign => mapDatabaseToPrettyCampaign(campaign));
+    });
+}
+
+export function saveCampaign(connection, prettyCampaign) {
+  const databaseCampaign = mapPrettyCampaignToDatabase(prettyCampaign);
+
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO campaigns (name) VALUES (?)', [
+      databaseCampaign.name,
+    ], (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(result);
+    });
+  })
+    .then(result => fetchCampaignById(connection, result.insertId));
 }
