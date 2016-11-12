331af3b51dc54e1d05bf8586e182515b8b4c2618
Primarily Snark <starkey.joshua@gmail.com>
2016-11-12 02:39:35 -0800

    Redesign login page for maximum confusion.
    Temporarily redesign login page to match the rest of the app. This
    needs better copy but for now it works.

--- src/app/shared/pages/home/index.js
+++ src/app/shared/pages/home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { TwentySidedDieIcon } from '../../components/icons/common-icons.g';
+
 import './styles.less';
 
 export const Home = () => (
   <div className="splash-page">
-    <div className="splash-page__tagline">Begin Your Adventure</div>
+    <div className="splash-page__logo">
+      <TwentySidedDieIcon />
+    </div>
     <div className="splash-page__actions">
       <a className="splash-page__actions__login" href="/auth/google">Sign in with Google+</a>
     </div>
